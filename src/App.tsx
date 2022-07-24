349275f980c1f36b7636085784107086ce78bc52
유병각 <bgyu@miridih.com>
2022-07-24 16:21:16 +0900

    feat: slotText 반복 타이밍 수정
    slotText 반복 타이밍 30ms 로수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import SlotText from "./components/View/SlotText"
+import "./css/font.css";
+import "./css/text.css";
 
 export default function App() {
   return(
-    <SlotText text={"abcde fffff"}></SlotText>
+    <SlotText text={"Your go-to interaction app for hybrid meetings"} classList={["Nunito", "title"]}></SlotText>
   )
 }
--- src/components/View/SlotText.tsx
+++ src/components/View/SlotText.tsx
@@ -5,30 +5,29 @@ import Random from "../../utils/Random";
 
 interface TextEffectPropsType {
     text: string;
+    classList?: string[];
 }
 
-export default function SlotText({text}: TextEffectPropsType) {
+export default function SlotText({text, classList}: TextEffectPropsType) {
     const [slotText, setSlotText] = useState(text);
     const [isPlaying, setIsPlaying] = useState(true);
     const [textIdx, setTextIdx] = useState(0);
     const [count, setCount] = useState(0);
 
     const slotTextEffect = () => {
-        if (count % 3 === 0) {
-            setTextIdx(textIdx + 1);
-        }
+        setTextIdx(textIdx + 1);
         if (textIdx >= text.length) {
             setIsPlaying(false);
         }
         setSlotText(text.substring(0, textIdx) + Random.getRandomText(slotText.substring(textIdx)));
-        setCount(count + 1);
+        
     }
 
-    useInterval(slotTextEffect, isPlaying ? 35 : null);
+    useInterval(slotTextEffect, isPlaying ? 30 : null);
 
     return(
-        <h1>
+        <p className={classList?.join(" ")}>
             {slotText}
-        </h1>
+        </p>
     )
 };
